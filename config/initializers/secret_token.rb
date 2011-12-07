b0ef8ca2f899d30000999298c026da9bcf9b0281
Clemens Grupp <clemens@videri-concept.de>
2011-12-07 16:38:46 +0100

    Rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linktool::Application.config.secret_token = '0914c003f3f6e88367d80cb9984d38e89b96bc9c97c9f0c1383708b6cdba836f514cfda2e94ab4f5e0844182ad37bc089d53f9098ca864099c7dbf6e0d8b77fc'
